36812b280d13fae223122ee8741615b94657a3d0
htetsu.aung <htetsu.aung@kbzbank.com>
2023-12-21 16:34:14 +0630

    generic style and data fetching hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,18 +2,26 @@ import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
-  const { games, error, loading } = useGames();
+  const { data, error, loading } = useGames();
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   return (
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10}>
-        {loading && skeletons.map((skeleton) => <GameCardSkeleton />)}
-        {games.map((game) => (
-          <GameCard game={game} />
+        {loading &&
+          skeletons.map((skeleton) => (
+            <GameCardContainer>
+              <GameCardSkeleton />
+            </GameCardContainer>
+          ))}
+        {data.map((game) => (
+          <GameCardContainer>
+            <GameCard game={game} />
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </>
